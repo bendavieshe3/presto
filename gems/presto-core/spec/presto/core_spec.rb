8ff99365e91f65dcbc28c89cc748d35aa7b8577d
Ben Davies <ben@bendavies.id.au>
2024-11-25 16:54:49 +1000

    Remove placeholder spec

--- gems/presto-core/spec/presto/core_spec.rb
+++ gems/presto-core/spec/presto/core_spec.rb
@@ -1,11 +1,8 @@
 # frozen_string_literal: true
+# FILE: gems/presto-core/spec/presto/core_spec.rb
 
 RSpec.describe Presto::Core do
   it "has a version number" do
     expect(Presto::Core::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
   end
 end
